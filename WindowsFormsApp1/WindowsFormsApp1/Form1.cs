8522270bb2afcc6f778fa9a354d2a2a70ebb922e
dabukofyuslesnoleyj <chinomanga@outlook.com>
2020-04-16 13:38:52 +0800

    Added functionality to remaining buttons

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -31,22 +31,26 @@ namespace WindowsFormsApp1
 
         private void closePicBtn_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null;
         }
 
         private void setBGbtn_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                 pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void closeBtn_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
         }
     }
 }
