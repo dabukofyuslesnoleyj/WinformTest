6c8f7384920bad684bbd4dcb3c2f24c477bab334
dabukofyuslesnoleyj <chinomanga@outlook.com>
2020-04-16 14:54:01 +0800

    import CSV Functionality

--- WindowsFormsAppIO/WindowsFormsAppIO/Form1.cs
+++ WindowsFormsAppIO/WindowsFormsAppIO/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using CsvHelper;
+using Json.Net;
 
 namespace WindowsFormsAppIO
 {
@@ -19,7 +22,19 @@ namespace WindowsFormsAppIO
 
         private void importCsvBtn_Click(object sender, EventArgs e)
         {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK){               
+                var reader = new StreamReader(openFileDialog1.FileName);
+                var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
 
+                var dt = new DataTable();
+                dt.Columns.Add("Id",typeof(int));
+                dt.Columns.Add("Name",typeof(string));
+                dt.Columns.Add("Progress",typeof(int));
+
+                dt.Load(csv);
+
+                dataGridView1.DataSource = dt;
+            }
         }
     }
 }
