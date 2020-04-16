f4fb402097dd1ef68f596923177b3ea44f81f5d3
xiaosean <xiaosean@users.noreply.github.com>
2020-04-16 16:11:01 +0800

    save Datagridview to Json

--- WindowsFormsAppIO/WindowsFormsAppIO/Form1.Designer.cs
+++ WindowsFormsAppIO/WindowsFormsAppIO/Form1.Designer.cs
@@ -28,22 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.progressChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.importCsvBtn = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.saveCsvBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressChart)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,7 +52,7 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.chart1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.progressChart, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
@@ -77,29 +76,28 @@
             this.dataGridView1.Size = new System.Drawing.Size(585, 498);
             this.dataGridView1.TabIndex = 0;
             // 
-            // chart1
+            // progressChart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(3, 3);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(584, 498);
-            this.chart1.TabIndex = 1;
-            this.chart1.Text = "chart1";
+            chartArea4.Name = "ChartArea1";
+            this.progressChart.ChartAreas.Add(chartArea4);
+            this.progressChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend4.Name = "Legend1";
+            this.progressChart.Legends.Add(legend4);
+            this.progressChart.Location = new System.Drawing.Point(3, 3);
+            this.progressChart.Name = "progressChart";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.progressChart.Series.Add(series4);
+            this.progressChart.Size = new System.Drawing.Size(584, 498);
+            this.progressChart.TabIndex = 1;
+            this.progressChart.Text = "progress chart";
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Controls.Add(this.importCsvBtn);
-            this.flowLayoutPanel1.Controls.Add(this.button2);
-            this.flowLayoutPanel1.Controls.Add(this.button3);
-            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.saveCsvBtn);
+            this.flowLayoutPanel1.Controls.Add(this.closeBtn);
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(593, 507);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -116,32 +114,25 @@
             this.importCsvBtn.UseVisualStyleBackColor = true;
             this.importCsvBtn.Click += new System.EventHandler(this.importCsvBtn_Click);
             // 
-            // button2
+            // saveCsvBtn
             // 
-            this.button2.Location = new System.Drawing.Point(426, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.saveCsvBtn.Location = new System.Drawing.Point(412, 3);
+            this.saveCsvBtn.Name = "saveCsvBtn";
+            this.saveCsvBtn.Size = new System.Drawing.Size(89, 23);
+            this.saveCsvBtn.TabIndex = 1;
+            this.saveCsvBtn.Text = "Save as JSON";
+            this.saveCsvBtn.UseVisualStyleBackColor = true;
+            this.saveCsvBtn.Click += new System.EventHandler(this.saveCsvBtn_Click);
             // 
-            // button3
+            // closeBtn
             // 
-            this.button3.Location = new System.Drawing.Point(345, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(264, 3);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.closeBtn.Location = new System.Drawing.Point(331, 3);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(75, 23);
+            this.closeBtn.TabIndex = 3;
+            this.closeBtn.Text = "Close";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
             // 
             // Form1
             // 
@@ -153,7 +144,7 @@
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressChart)).EndInit();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -163,12 +154,11 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart progressChart;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button importCsvBtn;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button saveCsvBtn;
+        private System.Windows.Forms.Button closeBtn;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
--- WindowsFormsAppIO/WindowsFormsAppIO/Form1.cs
+++ WindowsFormsAppIO/WindowsFormsAppIO/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 using CsvHelper;
 using Json.Net;
+using System.Globalization;
 
 namespace WindowsFormsAppIO
 {
@@ -20,21 +21,62 @@ namespace WindowsFormsAppIO
             InitializeComponent();
         }
 
+        private DataTable dataGridViewToDataTable(DataGridView dgv)
+        {
+            var dt = new DataTable();
+            foreach (DataGridViewColumn col in dgv.Columns)
+            {
+                dt.Columns.Add(col.Name);
+            }
+
+
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                DataRow dRow = dt.NewRow();
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    dRow[cell.ColumnIndex] = cell.Value;
+                }
+                dt.Rows.Add(dRow);
+            }
+
+            return dt;
+        }
+
         private void importCsvBtn_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK){               
                 var reader = new StreamReader(openFileDialog1.FileName);
                 var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+                var dr = new CsvDataReader(csv);
 
                 var dt = new DataTable();
                 dt.Columns.Add("Id",typeof(int));
                 dt.Columns.Add("Name",typeof(string));
                 dt.Columns.Add("Progress",typeof(int));
 
-                dt.Load(csv);
+                dt.Load(dr);
 
                 dataGridView1.DataSource = dt;
             }
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void saveCsvBtn_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var dt = dataGridViewToDataTable(dataGridView1);
+                string jsonData = JsonNet.Serialize(dt);
+
+                File.WriteAllText(saveFileDialog1.FileName+".json", jsonData);
+
+            }
+
         }
     }
 }
