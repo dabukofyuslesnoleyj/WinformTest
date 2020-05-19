1327adff9905ffc30057889cfc1177429633d08b
dabukofyuslesnoleyj <chinomanga@outlook.com>
2020-05-19 11:38:25 +0800

    Added an Adapter class to process a different json protocol

--- WindowsFormsAppSerialPort/WindowsFormsAppSerialPort/Sender.cs
+++ WindowsFormsAppSerialPort/WindowsFormsAppSerialPort/Sender.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace WindowsFormsAppSerialPort
 {
@@ -12,6 +14,42 @@ namespace WindowsFormsAppSerialPort
         public void send()
         {
             //TODO: implement sender function with sockets
+        }
+    }
+
+    //{
+    //MessageId : "0001",
+    //MessageType : "REQUEST"
+    //Message : "SET, <KEY>, <value>"
+    //}
+    interface IMessageAdapter
+    {
+        string translateMessage(string input);
+    }
+
+    class MaynardMessageAdapter : IMessageAdapter
+    {
+        public string translateMessage(string input)
+        {
+            Dictionary<string, string> inputData = JsonConvert.DeserializeObject<Dictionary<string, string>>(input);
+            Dictionary<string, string> outputData = new Dictionary<string, string>();
+            outputData.Add("commandID", inputData["MessageId"]);
+
+            string[] inputMessage = inputData["Message"].Split();
+            outputData.Add("commandType", inputMessage[0]);
+            switch (inputMessage[0])
+            {
+                case "SET":
+                    outputData.Add("messageTarget", inputMessage[1]);
+                    outputData.Add("messageType", "str");
+                    outputData.Add("messageValue", inputMessage[2]);
+                    break;
+                case "GET":
+                    outputData.Add("messageTarget", inputMessage[1]);
+                    break;
+            }
+
+            return JsonConvert.SerializeObject(outputData);
         }
     }
 }
